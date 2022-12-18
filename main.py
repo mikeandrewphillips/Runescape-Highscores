fd841dd09de132dc7656b8336d55749906a9ea1d
Michael Phillips <mikeandrewphillips@gmail.com>
2022-12-18 17:02:33 -0600

    Get data from OSRS API

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-change 
+import requests
+
+response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=PhysFighter')
+response_array = response.text.split("\n")
+print(response_array)

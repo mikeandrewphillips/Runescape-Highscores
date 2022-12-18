9da2c2ffef76c51befb3eea74686dafc89b8d0b1
Michael Phillips <mikeandrewphillips@gmail.com>
2022-12-18 17:45:11 -0600

    Refactor

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=PhysFighter')
-response_list = response.text.split("\n")
-#print(response_list)
+def get_highscores(username):
+    response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player='+username)
+    response_list = response.text.split("\n")
+    skill_names = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
+    skill_values = response_list[0:24]
+    skill_dictionary = dict(zip(skill_names,skill_values))
+    skill_dictionary = {k:v.split(",") for k,v in skill_dictionary.items()}
+    return skill_dictionary
 
-skill_names = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
-skill_values = response_list[0:24]
-skill_dictionary = dict(zip(skill_names,skill_values))
-skill_dictionary = {k:v.split(",") for k,v in skill_dictionary.items()}
-print(json.dumps(skill_dictionary))
+print(json.dumps(get_highscores("PhysFighter")))

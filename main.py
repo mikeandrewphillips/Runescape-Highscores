59a58a1d55df18eab0eddec6454c3ec97c6a3ea1
Michael Phillips <mikeandrewphillips@gmail.com>
2022-12-18 17:23:54 -0600

    Skil Dictionary Transformed to JSON string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=PhysFighter')
 response_list = response.text.split("\n")
@@ -6,5 +7,6 @@ response_list = response.text.split("\n")
 
 skill_names = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
 skill_values = response_list[0:24]
-skill_list = dict(zip(skill_names,skill_values))
-print(skill_list)
+skill_dictionary = dict(zip(skill_names,skill_values))
+skill_dictionary = {k:v.split(",") for k,v in skill_dictionary.items()}
+print(json.dumps(skill_dictionary))

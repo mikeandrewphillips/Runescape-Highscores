812ea6ca4ce91a8e43466ca888f78fe8435ab2e1
Michael Phillips <mikeandrewphillips@gmail.com>
2022-12-18 17:17:16 -0600

    Make Dictionary of Skills

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_
 response_list = response.text.split("\n")
 #print(response_list)
 
-skill_list = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
-print(len(skill_list))
+skill_names = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
+skill_values = response_list[0:24]
+skill_list = dict(zip(skill_names,skill_values))
+print(skill_list)

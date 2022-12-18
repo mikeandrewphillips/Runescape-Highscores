86a44228d46824b1a8708ca4067106ebde94e721
Michael Phillips <mikeandrewphillips@gmail.com>
2022-12-18 17:08:31 -0600

    Added Skills List

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
 response = requests.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=PhysFighter')
-response_array = response.text.split("\n")
-print(response_array)
+response_list = response.text.split("\n")
+#print(response_list)
+
+skill_list = ["Overall","Attack","Defense","Strength","Hitpoints","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction"]
+print(len(skill_list))
